c4fc23b5ef48aa35fdb3eda17b80d80d1990623b
coderxpb <borapankaj901@gmail.com>
2021-09-02 10:04:37 +0530

    Add basic onSearch handler

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.scss';
 import SearchContainer from './components/SearchContainer'
 
 function App() {
+
+	const handleSearch = (e) =>{
+		e.preventDefault();
+		console.log(e.target.value);
+	}
+
   return (
     <div className="App">
-			<SearchContainer/>
+			<SearchContainer onSearched={handleSearch}/>
     </div>
   );
 }
--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,8 +1,10 @@
-const SearchContainer = () => {
+import React from 'react';
+
+const SearchContainer = (props) => {
 	return (
 		<div className="search-container">
 			<p className="search-container-text">Manga similar to</p>
-			<input className="search-container-input" type="search" label="manga-search" placeholder="enter manga name here" />
+			<input className="search-container-input" type="search" label="manga-search" placeholder="enter manga name here" ref={e=>(e||{}).onsearch=props.onSearched}/>
 		</div>
 	)
 }
