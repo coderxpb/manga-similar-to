249fee4bc4d6c8ca88a70e346ea272462787be8d
coderxpb <borapankaj901@gmail.com>
2021-09-02 10:58:48 +0530

    Add basic async/await jikan api requests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.scss';
 import SearchContainer from './components/SearchContainer'
 
 function App() {
 
-	const handleSearch = (e) =>{
+	const handleSearch = async(e) =>{
 		e.preventDefault();
-		console.log(e.target.value);
+		try{
+			console.log(e.target.value);
+			const mangaSearch = await fetch(`https://api.jikan.moe/v3/search/manga?q=${e.target.value}&page=1`)
+			const mangaSearchResult = await mangaSearch.json();
+
+			const id = mangaSearchResult.results[0].mal_id;
+			const name = mangaSearchResult.results[0].title;
+			const img = mangaSearchResult.results[0].image_url;
+
+			const recommendationsPromise = await fetch(`https://api.jikan.moe/v3/manga/${id}/recommendations`);
+			const recommendationsResult = await recommendationsPromise.json();
+			console.log(recommendationsResult);
+		}catch(err){
+			console.log(err);
+		}
+
 	}
 
   return (
